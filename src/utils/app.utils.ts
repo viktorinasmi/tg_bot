fd4f4e6af63ff6d67d472245a454ec7f19f4a33c
viktoria <bika0724@gmail.com>
2023-01-29 19:15:55 +0300

    init and start tg_bot

--- src/utils/app.utils.ts
+++ src/utils/app.utils.ts
@@ -1,5 +1,5 @@
-export const showList = (todos) => {
-  `Твой список задач: \n\n ${todos
+export const showList = (todos: any[]) => {
+  return `Твой список задач: \n\n ${todos
     .map((todo) => (todo.isCompleted ? '✅' : '🔘') + ' ' + todo.name + '\n\n')
     .join('')}`;
 };
